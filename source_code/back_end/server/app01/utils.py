e231a31cf4e208abeebfac0eeb654e7d26158aae
Claire <118441317+lizhi-673366@users.noreply.github.com>
2023-05-20 12:39:16 +0930

    Add description of replace the API key.

--- source_code/back_end/server/app01/utils.py
+++ source_code/back_end/server/app01/utils.py
@@ -14,7 +14,15 @@ Use openai API to convert code into target coding language
 '''
 # check if raw_code is fromLanguage
 def checker(raw_code:str, fromLanguage: str) -> str:
-    
+    """
+    This function utilizes the OpenAI service for check the code language.
+    To replace the API key:
+    1. Visit the official website of OpenAI and sign in to your account.
+    2. Go to the API key section and generate a new key if you don't have one already.
+    3. Copy the new API key.
+    4. Replace the value of openai.api_key variable with the new API key.
+    5. Save the file and ensure the updated API key is used in subsequent API calls.
+    """
     openai.api_key = "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" # private API key
     model_engine = "text-davinci-002"  # Specify the model engine to use
 
@@ -36,7 +44,15 @@ def checker(raw_code:str, fromLanguage: str) -> str:
 
 # when the answer is yes, do converting
 def converter(raw_code:str, toLanguage:str) -> str:
-    
+    """
+    This function utilizes the OpenAI service for convert code.
+    To replace the API key:
+    1. Visit the official website of OpenAI and sign in to your account.
+    2. Go to the API key section and generate a new key if you don't have one already.
+    3. Copy the new API key.
+    4. Replace the value of openai.api_key variable with the new API key.
+    5. Save the file and ensure the updated API key is used in subsequent API calls.
+    """
     openai.api_key = "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" # private API key
     model_engine = "text-davinci-002"  # Specify the model engine to use
 
