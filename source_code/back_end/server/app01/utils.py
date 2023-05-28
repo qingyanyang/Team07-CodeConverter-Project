390babca7f5500655bba12b1388458d35d62a271
qingyan <yangqingyan0@gmail.com>
2023-05-28 14:43:34 +0930

    done check input code errors

--- source_code/back_end/server/app01/utils.py
+++ source_code/back_end/server/app01/utils.py
@@ -1,5 +1,40 @@
 import openai
 
+# common generator
+def generator(prompt:str)-> str:
+    """
+    This function utilizes the OpenAI service for convert code.
+    
+    To replace the API key:
+
+    1. Visit the official website of OpenAI and sign in to your account.
+    2. Go to the API key section and generate a new key if you don't have one already.
+    3. Copy the new API key.
+    4. Replace the value of openai.api_key variable with the new API key.
+    5. Save the file and ensure the updated API key is used in subsequent API calls.
+    """
+    openai.api_key = "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" # private API key
+    model_engine = "text-davinci-002"  # Specify the model engine to use
+    temperature = 0.5
+    max_tokens = 1000 # maximum char length
+
+    # Send the API request and print the generated text
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens
+    )
+
+    generated_text = response.choices[0].text.strip()
+    return generated_text
+
+
+# check if raw_code is fromLanguage
+def checker(raw_code:str, fromLanguage: str) -> str:
+    prompt = "is this " +raw_code+ " "+fromLanguage + " code? only reply \'Yes\' or \'No\'"
+    return generator(prompt)
+
 '''
 *Description:
 Use openai API to convert code into target coding language 
@@ -10,54 +45,22 @@ Use openai API to convert code into target coding language
 *ret:
     string generated_text : converted code in string format
 '''
-# check if raw_code is fromLanguage
-def checker(raw_code:str, fromLanguage: str) -> str:
-    #for developer: replace it ....with...
-    openai.api_key = "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" # private API key
-    model_engine = "text-davinci-002"  # Specify the model engine to use
-
-    prompt = "is this " +raw_code+ " "+fromLanguage + " code? only reply \'Yes\' or \'No\'"
-    print(prompt)
-    temperature = 0.5
-    max_tokens = 1000 # maximum char length
-
-    # Send the API request and print the generated text
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
-
-    generated_text = response.choices[0].text.strip()
-    return generated_text
-
-# when the answer is yes, do converting
 def converter(raw_code:str, toLanguage:str) -> str:
-    """
-    This function utilizes the OpenAI service for convert code.
-    To replace the API key:
-    1. Visit the official website of OpenAI and sign in to your account.
-    2. Go to the API key section and generate a new key if you don't have one already.
-    3. Copy the new API key.
-    4. Replace the value of openai.api_key variable with the new API key.
-    5. Save the file and ensure the updated API key is used in subsequent API calls.
-    """
-    openai.api_key = "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" # private API key
-    model_engine = "text-davinci-002"  # Specify the model engine to use
-
     prompt = "convert this code " +raw_code+ " to "+ toLanguage + "make it concise."
-    print(prompt)
-    temperature = 0.5
-    max_tokens = 1000 # maximum char length
+    return generator(prompt)
 
-    # Send the API request and print the generated text
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
+def bugChecker(raw_code:str) -> str:
+    prompt = "does this code " +raw_code+ " has errors? only reply \'Yes\' or \'No\'"
+    return generator(prompt)
 
-    generated_text = response.choices[0].text.strip()
-    return generated_text
+def identifier(raw_code:str) -> str:
+    prompt = "identify the programming language of this code " +raw_code + "only reply the language name"
+    return generator(prompt)
+
+def comparator(identifyLan:str,fromLanguage: str) -> str:
+    prompt = "are " + identifyLan + " and " + fromLanguage + " represent the same programming language? only reply \'Yes\' or \'No\'"
+    return generator(prompt)
+
+def corrector(raw_code:str) -> str:
+    prompt = "correct this code " + raw_code 
+    return generator(prompt)
--- source_code/back_end/server/app01/views.py
+++ source_code/back_end/server/app01/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render, HttpResponse
 # from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from urllib.parse import parse_qs
-from urllib.parse import unquote
 
-from app01.utils import checker, converter
+from app01.utils import checker, converter, bugChecker, identifier,comparator,corrector
 
 # from app01 import cpp2pyConvert
 # Create your views here.
@@ -40,7 +38,22 @@ def test(request):
 def convert_text(target,inputCode):
     return inputCode.upper() 
 
+def correct_fromLanguage(request):
+    if request.method == "POST":
+        print('收到需要correct的input')
+        body = request.body
 
+        # extract json file
+        data_str = body.decode('utf-8')
+        data = json.loads(data_str)
+
+        # get input code that need to be corrected
+        raw_code = data['raw_code']
+        print(raw_code)
+
+        #correct it and send it back to front-end
+        corrected_code = corrector(raw_code)
+        return HttpResponse(corrected_code)
 
 """
 Decorator enables to handle POST request
@@ -67,12 +80,23 @@ def code_converter_submit(request):
         fromLanguage = data['fromLanguage']
         print("fromLanguage",fromLanguage)
 
+        isBug = bugChecker(raw_code)
+        print("isBug",isBug)
+        # check bug
+        if str(isBug).startswith('Yes'):
+            # identify language even it has errors
+            identifyLan = identifier(raw_code)
+            print("identifyLan",identifyLan)
+            #Compare to fromLanguage
+            compareLan = comparator(str(identifyLan),fromLanguage)
+            return HttpResponse(compareLan)
+        # no bug
         # check if raw_code is fromLanguage
         ans = checker(raw_code, fromLanguage)
         print(ans)
         
-        # if it is, convert it and send to front-end
-        # if it is not, send "No" to front-end
+        # if yes, convert it and send to front-end
+        # if no, send "No" to front-end
         if str(ans).startswith('Yes'):
             code_converted = converter(raw_code,toLanguage)
             return HttpResponse(code_converted)
